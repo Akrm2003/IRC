bbebce7a289afa4fc860316a3bf7241bd4d721ed
alfakih <almohame@student.42abudhabi.ae>
2025-05-08 01:38:18 +0400

    starter

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,14 @@
 #define RED "\033[31m"
 #define RESET "\033[0m"
 
+
+// to run it ./ircserv 6667 testpassword
+// nc localhost 6667
+//try this basic command 
+
+//PASS testpassword
+// NICK testuser
+// USER testuser 0 * :Test User
 int main(int argc, char **argv) {
     try {
         if (argc != 3) {
